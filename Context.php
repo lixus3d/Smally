9d234b222b9edc4f5b075d6a942887cf230a1a18
Lixus3d <developpement@adreamaline.com>
2012-09-09 15:36:44 +0200

    Context object comments

--- Context.php
+++ Context.php
@@ -10,8 +10,8 @@ class Context {
 
 	/**
 	 * Construct the global $context object
-	 * @author Lixus3d <developpement@adreamaline.com>
-	 * @param array $vars
+	 * @param \Smally\Application $application reverse reference to the application
+	 * @param array $vars Context object $vars
 	 */
 	public function __construct(\Smally\Application $application, array $vars){
 		$this->setApplication($application);
@@ -20,28 +20,38 @@ class Context {
 		}
 	}
 
+	/**
+	 * Set the application reverse reference
+	 * @param \Smally\Application $application Current application linked to this object
+	 * @return \Smally\Context
+	 */
 	public function setApplication(\Smally\Application $application){
 		$this->_application = $application;
 		return $this;
 	}
 
+	/**
+	 * Return the application reverse referenced
+	 * @return \Smally\Application
+	 */
 	public function getApplication(){
 		return $this->_application;
 	}
+
 	/**
-	 * You can access $_REQUEST object style , and set element if you want ...
-	 * @author Lixus3d <developpement@adreamaline.com>
+	 * You can set $_REQUEST object style
 	 * @param string $name
 	 * @param mixed $value
+	 * @return mixed
 	 */
 	public function __set($name,$value){
 		return $this->_vars[$name] = $value;
 	}
 
 	/**
-	 * You can access $_REQUEST object style
-	 * @author Lixus3d <developpement@adreamaline.com>
+	 * You can get $_REQUEST object style
 	 * @param string $name
+	 * @return mixed
 	 */
 	public function __get($name){
 		return isset($this->_vars[$name]) ? $this->_vars[$name] : null;
