b8da7dc7ac9ba4be95757cad79df1446682a4b63
Lixus3d <developpement@adreamaline.com>
2012-09-09 15:31:37 +0200

    Config object comments

--- Config.php
+++ Config.php
@@ -4,6 +4,10 @@ namespace Smally;
 
 class Config {
 
+	/**
+	 * Config construct, you can give a $path to a config file (php array format)
+	 * @param mixed $path Path to a config file in php array format
+	 */
 	public function __construct($path=null){
 		if($path && is_file($path)){
 			require($path);
@@ -11,7 +15,11 @@ class Config {
 		}
 	}
 
-	public function setConfig($config){
+	/**
+	 * Define the config key => value in a recursive way
+	 * @param array $config An array of config value
+	 */
+	public function setConfig(array $config){
 		foreach($config as $key => $value){
 			if(is_array($value)){
 				$this->{$key} = new Config();
@@ -22,10 +30,19 @@ class Config {
 		}
 	}
 
+	/**
+	 * When a undefined key is __get return an empty Config object for compatibilty
+	 * @param  string $key
+	 * @return \Smally\Config Empty config object
+	 */
 	public function __get($key){
 		return new self();
 	}
 
+	/**
+	 * Return a empty string when converted to string
+	 * @return string
+	 */
 	public function __toString(){
 		return '';
 	}
