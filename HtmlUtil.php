743b9ee61d96bc9dcb39da3c995dd65b6b728c35
Lixus3d <developpement@adreamaline.com>
2012-09-09 15:45:35 +0200

    HtmlUtil object comments

--- HtmlUtil.php
+++ HtmlUtil.php
@@ -4,21 +4,28 @@ namespace Smally ;
 
 class HtmlUtil {
 
-	static public function toAttributes($attributes,$enclosure='"',$equal='=',$sep=' '){
+	/**
+	 * Convert an array of key => values to xml/html attributes
+	 * @param  array $attributes Array of key => values
+	 * @param  string $enclosure  enclose attribute with this char
+	 * @param  string $equal      equal sign char
+	 * @param  string $sep        separator between attributes
+	 * @return string
+	 */
+	static public function toAttributes(array $attributes,$enclosure='"',$equal='=',$sep=' '){
 		$str = "";
 		if (is_array($attributes)) {
 			$str .= $sep;
 			foreach($attributes as $key => $value) {
 				if(is_array($value)) $value = implode(' ',$value); // case of "class" is practical under array type
 				switch (true) {
-					case (strlen($value) > 0) :
-					case $key == 'action' :
+					case (strlen($value) > 0) : // a value must be defined , except for special $key
+					case $key == 'action' : // action key has valid empty value
 						$str .= $key . $equal . $enclosure . $value . $enclosure .$sep;
 						break;
 				}
 			}
 		}
-
 		return rtrim($str);
 	}
 
