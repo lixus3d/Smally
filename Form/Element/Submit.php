c274f0b2d08d6007b99344670d6e82522a6affd9
Lixus3d <developpement@adreamaline.com>
2012-09-15 20:57:23 +0200

    Form : Fix submit populateValue logic

--- Form/Element/Submit.php
+++ Form/Element/Submit.php
@@ -6,4 +6,12 @@ class Submit extends AbstractElement{
 
 	protected $_type = 'submit';
 
+	/**
+	 * There isn't logic to populate a Submit
+	 * @param  string $value Irrevelant for submit element
+	 * @return \Smally\Form\Element\AbstractElement
+	 */
+	public function populateValue($value){
+		return $this;
+	}
 }
