0f130ff32aa8f3f2538798e68b2ef950f6ebddcf
Lixus3d <developpement@adreamaline.com>
2012-09-24 23:35:44 +0200

    Add auto active, alpha, omega classes

--- Helper/Decorator/Menu.php
+++ Helper/Decorator/Menu.php
@@ -5,19 +5,41 @@ namespace Smally\Helper\Decorator;
 class Menu extends AbstractDecorator {
 
 	/**
+	 * Return the element (menu) attributes
+	 * @return [type] [description]
+	 */
+	public function getAttributes(){
+		return $this->getElement()->getAttributes();
+	}
+
+	/**
 	 * Render the menu decorator
+	 * Usually the only one redefine in another Menu Decorator
 	 * @param  string $content Existing generated content
 	 * @return string
 	 */
 	public function render($content=''){
-		$attributes = $this->getElement()->getAttributes();
-		$menu = $this->getElement();
-
-		$html = '<ul'.\Smally\HtmlUtil::toAttributes($attributes).'>' . NN;
-		foreach($this->getElement()->getItems() as $key => $item){
-			$html .= $this->getElement()->getDecorator('menuElement',$item)->setMenu($menu)->render() . NN;
-		}
+		$html  = '<ul'.\Smally\HtmlUtil::toAttributes($this->getAttributes()).'>' . NN;
+		$html .= $this->renderChildren();
 		$html .= '</ul>' . NN;
-		return $html ;
+		return $this->concat($html,$content);
+	}
+
+
+	/**
+	 * Return the render of the current element sub elements
+	 * @return string
+	 */
+	public function renderChildren(){
+		$html = '';
+		$items = $this->getElement()->getItems();
+		foreach($items as $key => $item){
+			$html .= $this->getElement()
+							->getDecorator('menuElement',$item)
+								->setMenu($this->getElement())
+								->setElementNumber($key,count($items))
+								->render();
+		}
+		return $html;
 	}
 }
--- Helper/Decorator/MenuElement.php
+++ Helper/Decorator/MenuElement.php
@@ -4,42 +4,106 @@ namespace Smally\Helper\Decorator;
 
 class MenuElement extends AbstractDecorator {
 
+	protected $_elementNumber = null;
+	protected $_elementTotal = null;
+
 	protected $_menu = null;
 
+	protected $_classActive = 'active';
+	protected $_classAlpha 	= 'alpha';
+	protected $_classOmega  = 'omega';
+
+	/**
+	 * Define the back reference to the Menu Helper
+	 * @param \Smally\Helper\Menu $menu the Menu
+	 * @return \Smally\Helper\Decorator\MenuElement
+	 */
 	public function setMenu(\Smally\Helper\Menu $menu){
 		$this->_menu = $menu;
 		return $this;
 	}
 
-	public function getMenu(){
-		return $this->_menu;
+	/**
+	 * Define the actual element number (usually for element alpha / omega)
+	 * @param integer $k the element number
+	 */
+	public function setElementNumber($k=null,$total=0){
+		if(is_null($k)) $k = ++$this->_elementNumber;
+		$this->_elementNumber = $k;
+		$this->_elementTotal = $total;
+		return $this;
 	}
 
 	/**
-	 * Render the menu decorator
-	 * @param  string $content Existing generated content
-	 * @return string
+	 * Return the Helper\Menu called for this element
+	 * @return \Smally\Helper\Menu
 	 */
-	public function render($content=''){
+	public function getMenu(){
+		return $this->_menu;
+	}
+		/**
+	 * Get the line number of the decorator
+	 * @return integer
+	 */
+	public function getElementNumber(){
+		return self::$_elementNumber;
+	}
 
+	/**
+	 * Return the element attributes, mix between the ones in Menu helper and the one in the element itself
+	 * @return array
+	 */
+	public function getAttributes(){
 		$attributes = $this->getElement()->getAttributes(); // Attributes of the current element , specific id or rel for example
 		if($menu = $this->getMenu()){
 			$attributes = array_merge($this->getMenu()->getAttributesElement(),$attributes); // Attributes of the menu element, a generic class for
 		}
 
-		$html  = '<li'.\Smally\HtmlUtil::toAttributes($attributes).'>';
+		// Add the active automatically
+		if($application = \Smally\Application::getInstance()){
+			$actualUrl = $application->getRooter()->getActualUrl();
+			if($this->getElement()->getUrl() == $actualUrl){
+				$attributes['class'][] = $this->_classActive;
+			}
+		}
+
+		// Add alpha and omega automatically
+		if($this->_elementNumber === 0) $attributes['class'][] = $this->_classAlpha;
+		if($this->_elementNumber === ($this->_elementTotal-1)) $attributes['class'][] = $this->_classOmega;
+
+		return $attributes;
+	}
+
+	/**
+	 * Render the menu decorator
+	 * Usually the only one redefine in another MenuElement Decorator
+	 * @param  string $content Existing generated content
+	 * @return string
+	 */
+	public function render($content=''){
+		$html  = '<li'.\Smally\HtmlUtil::toAttributes($this->getAttributes()).'>';
 		$html .= '<a href="'.$this->getElement()->getUrl().'">';
+		$html .= '<span>';
 		$html .= '<span>';
 		$html .= $this->getElement()->getName();
 		$html .= '</span>';
+		$html .= '</span>';
 		$html .= '</a>';
+		$html .= $this->renderChildren();
+		$html .= '</li>' . NN;
+		return $this->concat($html,$content);
+	}
+
+	/**
+	 * Return the render of the element sub elements
+	 * @return string
+	 */
+	public function renderChildren(){
 		if($this->getElement()->hasChildren()){ // if we have subchildren
 			$subMenu = clone $this->getMenu(); // clone the menu generator to copy the parent menu behaviors and attributes
 			$subMenu->setTree($this->getElement()); // set the correct tree
-			$html .= $subMenu ->render(); // render the sub menu in the actual menuElement
+			return $subMenu ->render(); // render the sub menu in the actual menuElement
 		}
-		$html .= '</li>' . NN;
-
-		return $html ;
+		return '';
 	}
 }
