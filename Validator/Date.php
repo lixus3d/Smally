98d2df47325a4da8878ca0c6b01892fd5be52a4e
Lixus3d <developpement@adreamaline.com>
2012-10-11 21:52:50 +0200

    Add date format validator

---
+++ Validator/Date.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Smally\Validator;
+
+class Date extends AbstractRule {
+
+	protected $_format = null;
+
+	protected static $_validFormat = array(
+			'fr',
+			'en',
+			'int',
+			'unix'
+		);
+
+	/**
+	 * Construct the rule with options
+	 * @param string  $format    format of the expected date (fr, en, int)
+	 */
+	public function __construct($format='fr'){
+		$this->setFormat($format);
+	}
+
+	/**
+	 * Set the expected format
+	 * @param string $format
+	 * @return \Smally\Validator\Date
+	 */
+	public function setFormat($format){
+		if(in_array($format, self::$_validFormat)){
+			$this->_format = $format;
+		}else throw new \Smally\Exception('Invalid date validator format !');
+		return $this;
+	}
+
+	/**
+	 * Validate if the $valueToTest is of the attended date $format
+	 * @param  mixed $valueToTest
+	 * @return boolean
+	 */
+	public function x($valueToTest){
+
+		$valueToTest = (string) $valueToTest;
+
+		switch($this->_format){
+			default:
+			case 'fr':
+				$regex = '#^(0[1-9]|[12][0-9]|3[01])([/.])(0[1-9]|1[0-2])\\2([0-9]{2}|[0-9]{4})$#';
+				if(preg_match($regex,$valueToTest,$matches)){
+					return true;
+				}else $this->addError('Le format de date JJ/MM/AAAA n\'est pas respecté');
+			break;
+			case 'en':
+				$regex = '#^(0[1-9]|1[0-2])([/.])(0[1-9]|[12][0-9]|3[01])\\2([0-9]{2}|[0-9]{4})$#';
+				if(preg_match($regex,$valueToTest,$matches)){
+					return true;
+				}else $this->addError('Le format de date MM/JJ/AAAA n\'est pas respecté');
+			break;
+			case 'int':
+				$regex = '#^([0-9]{2}|[0-9]{4})([/.])(0[1-9]|1[0-2])\\2(0[1-9]|[12][0-9]|3[01])$#';
+				if(preg_match($regex,$valueToTest,$matches)){
+					return true;
+				}else $this->addError('Le format de date AAAA/MM/JJ n\'est pas respecté');
+			break;
+			case 'unix':
+				$regex = '#^([0-9]{1,})$#';
+				if(preg_match($regex,$valueToTest,$matches)){
+					return true;
+				}else $this->addError('Le format de date XXXXXXXX n\'est pas respecté');
+			break;
+		}
+		return false;
+	}
+
+}
