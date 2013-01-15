e568010dd1e74defab5ebe0ca860cf77c7bae46a
Lixus3d <developpement@adreamaline.com>
2013-01-15 17:29:18 +0100

    Add a jsMultiselect possibility

--- Form/Element/Multiselect.php
+++ Form/Element/Multiselect.php
@@ -7,4 +7,15 @@ class Multiselect extends Radio{
 	protected $_type = 'multiselect';
 	protected $_decorator = 'select';
 
+	public function setModeJs(){
+		$this->setAttribute('class','jsMultiselect');
+		if($app = \Smally\Application::getInstance()){
+			$app
+				->setJs('js/jquery.min.js')
+				->setJs('js/bootstrap-multiselect.js')
+				->setJs('js/smally/form/Multiselect.js')
+				;
+		}
+	}
+
 }
