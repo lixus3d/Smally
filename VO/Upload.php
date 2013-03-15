210c861401203c422699fbf3d9aaf7e556cc2bd8
Lixus3d <developpement@adreamaline.com>
2013-03-15 22:50:46 +0100

    Fix extension detection when in tmp dir

--- VO/Upload.php
+++ VO/Upload.php
@@ -101,7 +101,15 @@ class Upload extends \Smally\VO\Standard {
 	 * @return string
 	 */
 	public function getExtension(){
-		return strtolower(substr(strrchr($this->filePath,'.'),1));
+		// try to get it from filePath, if tmp, get it from name
+		$ext = strtolower(substr(strrchr($this->filePath,'.'),1));
+		if($ext == 'tmp'){
+			$extName = strtolower(substr(strrchr($this->name,'.'),1));
+			if($extName !== 'tmp' && preg_match('#^[0-9a-z.-]+$#', $extName)){
+				$ext = $extName;
+			}
+		}
+		return $ext;
 	}
 
 	/**
